7d2d3fa859bc83018672bfd813525201c8189ab2
Boloom <vseree@enssat.fr>
2022-03-20 14:08:39 +0100

    Ajout deux pages Twitter + Investigation (Marvel vs DC)

--- appBDD/views.py
+++ appBDD/views.py
@@ -36,20 +36,44 @@ def droitscine():
 def evolution_marvelcomics():
     return render_template('/investigation/evolutionmarvel.html')
 
+# Page Marvel vs DC Comics (Investigation)
+@app.route('/investigation/marvel-vs-dc')
+def marvel_vs_dc():
+    return render_template('/investigation/marvelvsdc.html')
+
 # Page Sciences
 @app.route('/sciences')
 def sciences():
     return render_template('/sciences.html')
 
 # Page Sciences
-@app.route('/twitter')
+@app.route('/treeter')
 def twitter():
-    return render_template('/sites/twitter/accueil.html')
+    return render_template('/sites/twitter/accueil.html', mode="light")
+
+@app.route('/twitterDark')
+def twitterDark():
+    return render_template('/sites/twitter/accueil.html', mode="dark")
 
 # Page Sciences
 @app.route('/profil')
 def twitter_profil():
-    return render_template('/sites/twitter/profil.html')
+    return render_template('/sites/twitter/profil.html', mode="light")
+
+# Profil Twitter de Ben Urich
+@app.route('/treeter/urich4thetruth')
+def twitter_profil_ben_urich():
+    return render_template('/sites/twitter/profilBenUrich.html', mode="light")
+
+# Profil Twitter de Ben Urich
+@app.route('/treeter/thew4tcher')
+@app.route('/treeter/profile')
+def twitter_profil_user():
+    return render_template('/sites/twitter/profilUser.html', mode="light")
+
+@app.route('/profilDark')
+def twitter_profilDark():
+    return render_template('/sites/twitter/profil.html', mode="dark")
 
 # Page Grandir comme Giant-Man (Sciences)
 @app.route('/sciences/grandir-comme-giantman')
